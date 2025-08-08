e956f532d48b14bb80044820ff5f1966722a4261
mahmoudAhmed122001 <m.lebdah@tocaan.com>
2025-08-08 13:03:49 +0300

    feat: Add onboarding and splash screens with localization support
    - Introduced a new background image for the onboarding screens.
    - Updated Arabic and English translations with new keys for onboarding.
    - Created AppColor class for centralized color management.
    - Implemented OnboardingScreen with carousel slider and dots indicator for navigation.
    - Added SplashScreen with Lottie animation and navigation to OnboardingScreen.
    - Updated main.dart to set SplashScreen as the initial route.
    - Added necessary dependencies for carousel slider, dots indicator, and Lottie animations.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:meals_app/features/onboarding/onboarding_screen.dart';
+import 'package:meals_app/features/onboarding/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,85 +35,7 @@ class MyApp extends StatelessWidget {
           home: child,
         );
       },
-      child: const HomeScreen(),
+      child: const SplashScreen(),
     );
   }
 }
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  void changeLanguage() {
-    if (context.locale == Locale('en')) {
-      context.setLocale(Locale('ar'));
-    } else {
-      context.setLocale(Locale('en'));
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.sizeOf(context);
-    print('------build------');
-    return Scaffold(
-      body: SizedBox(
-        width: size.width,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(context.locale.toString(), style: TextStyle(fontSize: 30.sp)),
-
-            Text('hello'.tr(), style: TextStyle(fontSize: 30.sp)),
-
-            Text('my_meals'.tr(), style: TextStyle(fontSize: 30.sp)),
-
-            Text('test'.tr(), style: TextStyle(fontSize: 30.sp)),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: changeLanguage,
-        child: const Icon(Icons.language),
-      ),
-    );
-  }
-}
-
-
-
-// SizedBox(
-//         width: size.width,
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             isNetworkImage == true
-//                 ? Image.network(
-//                     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXZylLZLdEOnpA7xCFv_tEqFvcThCY70wK7Q&s',
-//                   )
-//                 : Image.asset('assets/pngs/food.jpg'),
-//             SizedBox(height: 50.h),
-//             TextButton(
-//               onPressed: () {
-//                 setState(() {
-//                   isNetworkImage = !isNetworkImage;
-//                 });
-
-//                 print('isNetworkImage: $isNetworkImage');
-//               },
-//               child: Text('convert'),
-//             ),
-//             TextButton(
-//               onPressed: () {
-//                 setState(() {});
-//               },
-//               child: Text('test button'),
-//             ),
-//           ],
-//         ),
-//       ),
-   
